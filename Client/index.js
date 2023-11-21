7d64ee85b980b0e59f92ca79cd127a4af12a92a6
naffiziqbal <f.nafiziqbal@gmail.com>
2023-11-21 16:32:17 +0600

    Adding Message Feature

--- Client/index.js
+++ Client/index.js
@@ -1,9 +1,20 @@
-const senderContent = document.querySelector(".senderMessage");
+import { io } from "socket.io-client";
+
+// const senderContent = document.querySelector(".senderMessage");
 const viewerContent = document.querySelector(".viewerMessage");
 const form = document.getElementById("form");
 // const room = document.getElementById("room-input");
 
-senderContent.textContent = "Hello";
+// senderContent.textContent = "Hello";
+
+const socket = io("http://localhost:3000");
+socket.on("connect", () => {
+  displayProfileInfo(`Youre Connected with id : ${socket.id}`);
+});
+
+socket.on("reciveMessage", (message) => {
+  displayViewerMessage(message);
+});
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -11,15 +22,27 @@ form.addEventListener("submit", (e) => {
   // const room = roomInput.value;
 
   if (message === "") return;
-  displayMessage(message);
+  displaySenderMessage(message);
+  socket.emit("sendMessage", message);
   form.message.value = "";
 });
 
+const displaySenderMessage = (message) => {
+  const div = document.createElement("div");
+  div.textContent = message;
+  document.querySelector(".mainMessageBody").append(div);
+  div.classList.add("senderMessage");
+};
 
-const displayMessage = (message) => {
-  const div = document.createElement('div')
-  div.textContent = message
-  document.querySelector('.sender').append(div)
-  div.classList.add('senderMessage')
+const displayViewerMessage = (message) => {
+  const div = document.createElement("div");
+  div.textContent = message;
+  document.querySelector(".mainMessageBody").append(div);
+  div.classList.add("viewerMessage");
+};
 
-}
+const displayProfileInfo = (info) => {
+  const div = document.createElement("div");
+  div.textContent = info;
+  document.querySelector(".profileInfo").append(div);
+};
