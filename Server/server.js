fa355cd47f66320be9300928716c77740f188c3a
naffiziqbal <f.nafiziqbal@gmail.com>
2023-11-23 23:23:05 +0600

    Room

--- Server/server.js
+++ Server/server.js
@@ -17,18 +17,23 @@ app.get("/", (req, res) => {
   res.send("Server is online");
 });
 
-io.on("connection", (socket) => {
-  console.log("User Connected");
+io.on("connection", async (socket) => {
+  // socket.on("sending-room", (room) => {
+  //   socket.join(room);
+  //   console.log("User Connected on ", room);
+  // });
+
   console.log(socket.id);
   socket.on("sendMessage", (message, room) => {
-    console.log(room, " room");
+    console.log(room)
+    console.log(message)
     socket.join(room)
-    if (room === "" || null) {
-      return;
+    if (room === "" || null) {      return;
     } else {
       socket.to(room).emit("reciveMessage", message);
     }
   });
+
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
@@ -36,7 +41,7 @@ io.on("connection", (socket) => {
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => console.log("Listerning on port"));
+server.listen(PORT, () => console.log(`Listerning on port ${PORT}`));
 
 // app.res
 module.export = app;
